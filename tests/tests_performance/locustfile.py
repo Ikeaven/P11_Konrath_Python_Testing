9858ba803df18167c47aa4ba08444aa36230ee69
Konrath <36306272+Ikeaven@users.noreply.github.com>
2022-01-17 18:04:02 +0100

    test/test performance with locust

---
+++ tests/tests_performance/locustfile.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def test_index(self):
+        self.client.get("/")
+
+    @task
+    def test_clubs_list(self):
+        self.client.get("/clubs_list")
+
+    @task
+    def test_showSummary(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
